2dbe891c932810f5f8a831f5adf83b41e7faf972
Hama Nsta <hamalaw4@gmail.com>
2026-05-13 22:58:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'hama-nista-v1';
+const assets = [
+  'index.html',
+  'profile.jpg',
+  'icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
